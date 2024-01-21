5fa689c4e77dd1af5139a5f6ef900ce2f9282a78
Jean-Paul <jean.paul.bassil@outlook.com>
2024-01-21 21:18:14 +0200

    add: added details template

--- polls/views.py
+++ polls/views.py
@@ -4,4 +4,18 @@ from django.shortcuts import render
 from .models import Question, Choice
 
 def index(request):
-    return render(request, 'polls/index.html')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+
+    return render(request, 'polls/index.html', context=context)
+
+def detail(request, question_id):
+    try:
+        question = Question.object.get(pk=question_id)
+    except Question.DoesNotExist: 
+        raise Http404("Question does not exist")
+    
+    return render(request, 'polls/results.html', {'question': question})
